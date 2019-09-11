8d91155203b83e18992574fe87a8fe94e96bb2d5
NuthanChandra <nuthanchandra1997@gmail.com>
2019-09-11 12:02:33 +0530

    Add jsx folder

---
+++ jsx/src/index.js
@@ -0,0 +1,8 @@
+// Import the React and ReactDOM libraries
+import React from 'react'; // AFter import is the variable, it can be anything. After from is the import file
+import ReactDOM from 'react-dom'
+
+// Create a React component
+
+
+// Take the React component and show it on the screen
