90bd13aa067658e74d29248ae88811bb000e589a
NuthanChandra <nuthanchandra1997@gmail.com>
2019-09-17 09:00:15 +0530

    Include correct JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -6,9 +6,9 @@ import ReactDOM from 'react-dom'
 const App = () => {
     return (
     <div>
-        <label class="label" for="name">Enter name:</label>
+        <label className="label" htmlFor="name">Enter name:</label>
         <input id="name" type="text" />
-        <button style="background-color: blue;color:white;">Submit</button>
+        <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
     </div>
     );
 }
