acade4512ec1da70431578702d02169997ddcd57
NuthanChandra <nuthanchandra1997@gmail.com>
2019-09-17 08:30:38 +0530

    Add index.js

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -3,6 +3,18 @@ import React from 'react'; // AFter import is the variable, it can be anything. 
 import ReactDOM from 'react-dom'
 
 // Create a React component
-
+const App = () => {
+    return (
+    <div>
+        <label class="label" for="name">Enter name:</label>
+        <input id="name" type="text" />
+        <button style="background-color: blue;color:white;">Submit</button>
+    </div>
+    );
+}
 
 // Take the React component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
