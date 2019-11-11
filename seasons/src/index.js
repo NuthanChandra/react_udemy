49333724f61fab29881e66c57550f8d45e45c6c6
NuthanChandra <nuthanchandra1997@gmail.com>
2019-11-11 09:42:20 +0530

    Move position code as render is called very frequently

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -5,12 +5,12 @@ class App extends React.Component {
     constructor(props){
         super(props);
         this.state = {lat: null};
-    }
-    render(){
         navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
             (err) => console.log(err)
         );
+    }
+    render(){
         return <div>Latitude: {this.state.lat}</div>
     }
 }
