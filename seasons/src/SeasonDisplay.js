55d1870e6f91e88b3b2bf4a924865549b0db67c2
NuthanChandra <nuthanchandra1997@gmail.com>
2019-11-15 10:01:40 +0530

    Author's way of refactoring icons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -20,11 +20,12 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props)=> {
     const season = getSeason(props.lat, new Date().getMonth());
+    const { text, iconName } = seasonConfig[season];
     return (
     <div>
-        <i className={`${seasonConfig[season].iconName} icon`} />
-        <h1>{seasonConfig[season].text}</h1>
-        <i className={`${seasonConfig[season].iconName} icon`} />
+        <i className={`${iconName} icon`} />
+        <h1>{text}</h1>
+        <i className={`${iconName} icon`} />
     </div>
     );
 };
