3afede8a34c5206e0ee2749248ed9068d9e610f1
NuthanChandra <nuthanchandra1997@gmail.com>
2019-11-22 11:26:06 +0530

    Further condensation of event handler syntax

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -7,7 +7,7 @@ class SearchBar extends React.Component {
                 <form className="ui form"> 
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={(event) => console.log(event.target.value)}/>
+                        <input type="text" onChange={(e) => console.log(e.target.value)}/>
                     </div>
                 </form>
             </div>
