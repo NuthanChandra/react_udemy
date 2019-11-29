56dc3ae044d3dc459beafb0c6c5791f9bbc85144
NuthanChandra <nuthanchandra1997@gmail.com>
2019-11-29 09:45:02 +0530

    Same issue of 'this'

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,24 +3,27 @@ import axios from "axios";
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    async onSearchSubmit(term){
-        const response = await axios.get("https://api.unsplash.com/search/photos", {
-          params: { query: term },
-          headers: {
-            Authorization:
-              "Client-ID b21f7c6cf1f472381f90a5874ad62e529acb2af8e8a737eedd35089a57d17abd"
-          }
-        });  
-        console.log(response.data.results);
-    }
+  state = { images: [] };
 
-    render(){
-        return(
-          <div className="ui container" style={{ marginTop: "10px" }}>
-            <SearchBar onSubmit={this.onSearchSubmit}/>
-          </div>
-        );
-    }
+  async onSearchSubmit(term){
+      const response = await axios.get("https://api.unsplash.com/search/photos", {
+        params: { query: term },
+        headers: {
+          Authorization:
+            "Client-ID b21f7c6cf1f472381f90a5874ad62e529acb2af8e8a737eedd35089a57d17abd"
+        }
+      });  
+      this.setState({ images: response.data.results });
+  }
+
+  render(){
+      return(
+        <div className="ui container" style={{ marginTop: "10px" }}>
+          <SearchBar onSubmit={this.onSearchSubmit}/>
+          Found: {this.state.images.length} images
+        </div>
+      );
+  }
 }
 
 export default App;
