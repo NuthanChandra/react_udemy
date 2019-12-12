0d33fa57b6f0481469f886895e5f62175eed8afc
NuthanChandra <nuthanchandra1997@gmail.com>
2019-12-12 11:00:35 +0530

    Add semantic-ui css

--- my_buildings/src/components/App.js
+++ my_buildings/src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class App extends React.Component {
     render(){
-        return <div>Hi there, I am App</div>
+        return <div className="ui container">Hi there, I am App</div>
     }
 }
 
