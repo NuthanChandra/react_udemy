a5882d208efa531bf61bcdaf10b2312713dac29e
NuthanChandra <nuthanchandra1997@gmail.com>
2019-12-13 09:54:49 +0530

    Unstage unwanted dirs

--- pics/src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-    Authorization:
-      "Client-ID b21f7c6cf1f472381f90a5874ad62e529acb2af8e8a737eedd35089a57d17abd"
-  }
-});
